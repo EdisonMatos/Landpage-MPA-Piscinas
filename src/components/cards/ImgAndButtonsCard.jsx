ff1f6e5977a4d7eaa43a6fa18aa449daa6dece8f
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-01-12 16:25:11 -0300

    fix: Deixar cards do mesmo tamanho

--- src/components/cards/ImgAndButtonsCard.jsx
+++ src/components/cards/ImgAndButtonsCard.jsx
@@ -1,20 +1,20 @@
-import PropTypes from "prop-types";
-import MotionDivLeftToRight from "../animation/MotionDivLeftToRight";
-import { Link } from "react-router-dom";
+import PropTypes from 'prop-types'
+import MotionDivLeftToRight from '../animation/MotionDivLeftToRight'
+import { Link } from 'react-router-dom'
 
 export default function ImgAndButtonsCard(props) {
-  const { img, alt, button1Label, button1Link } = props;
+  const { img, alt, button1Label, button1Link } = props
 
   const handleClick = () => {
-    window.location.href = button1Link + "#";
-  };
+    window.location.href = button1Link + '#'
+  }
 
   ImgAndButtonsCard.propTypes = {
     img: PropTypes.any,
     alt: PropTypes.string,
     button1Label: PropTypes.any,
     button1Link: PropTypes.any,
-  };
+  }
 
   return (
     <div className="w-full phone3:w-[45%] desktop1:w-[23%]  bg-white p-[15px] my-[14px] rounded-[10px] flex flex-col gap-[15px]">
@@ -26,9 +26,11 @@ export default function ImgAndButtonsCard(props) {
 
       <Link onClick={handleClick} className="w-full">
         <button className="text-white bg-red-700 rounded-[10px] text-paragraph3 py-[8px] px-[31px] font-mainFont font-medium w-full transition hover:bg-red-900">
-          <MotionDivLeftToRight><p>{button1Label}</p></MotionDivLeftToRight>
+          <MotionDivLeftToRight>
+            <p>{button1Label}</p>
+          </MotionDivLeftToRight>
         </button>
       </Link>
     </div>
-  );
+  )
 }
