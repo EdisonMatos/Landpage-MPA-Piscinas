6c37a71fcea0a05f4fb6d8671b69d44cbb01d82a
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-01-19 17:10:57 -0300

    fix: Moldura UI cards

--- src/components/cards/ImgCodeDescButtonCard.jsx
+++ src/components/cards/ImgCodeDescButtonCard.jsx
@@ -14,7 +14,7 @@ export default function ImgCodeDescButtonCard(props) {
   }
 
   return (
-    <div className="w-full phone3:w-[46%] tablet1:w-[47%] tablet2:w-[30.5%] desktop1:w-[30.6%] desktop3:w-[31%] min-h-[410px] bg-white p-[15px] rounded-[10px] flex flex-col justify-between gap-[10px]">
+    <div className="w-full relative phone3:w-[46%] tablet1:w-[47%] tablet2:w-[30.5%] desktop1:w-[30.6%] desktop3:w-[31%] min-h-[410px] bg-white p-[15px] rounded-[10px] flex flex-col justify-between gap-[10px] overflow-hidden">
       <div className="flex justify-center items-center h-[200px] phone3:h-[200px] tablet1:h-[200px]">
         <img src={img} alt={alt} className="max-h-[190px] max-w-[150px]" />
       </div>
